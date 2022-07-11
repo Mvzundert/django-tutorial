b8bc9862b37f69328f0ed194e827d593fe55ecc5
Marius van Zundert <marius.vanzundert@cerberos.dev>
2022-07-11 11:41:28 +0200

    Part 4: Minimal Forms

--- polls/models.py
+++ polls/models.py
@@ -18,6 +18,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+    DoesNotExist = None
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
